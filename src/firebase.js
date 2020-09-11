6f0b8ce133812ca9e872cf56b9859a1da6b99950
smitptl <smitvptl@gmail.com>
2020-09-11 16:00:54 -0700

    build amazon front page

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDNMAXZWe002PQRq1ogTw1uZT92DebNqRQ",
+    authDomain: "challenge-a4c00.firebaseapp.com",
+    databaseURL: "https://challenge-a4c00.firebaseio.com",
+    projectId: "challenge-a4c00",
+    storageBucket: "challenge-a4c00.appspot.com",
+    messagingSenderId: "207159541283",
+    appId: "1:207159541283:web:e19d75e1c0679d74dd3b96",
+    measurementId: "G-ZZB7ZGC0JZ"
+};
